74020ea7035e2f60a3bf59524ec39c265e853397
juanpetterson <juan.petterson@gmail.com>
2020-03-31 17:47:03 -0300

    Implemented auth guard with snack notification

---
+++ src/app/user/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { SnackService } from '../services/snack.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private snackService: SnackService
+  ) {}
+
+  async canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    const user = await this.afAuth.auth.currentUser;
+    const isLoggedIn = !!user;
+
+    if (!isLoggedIn) {
+      this.snackService.authError();
+    }
+
+    return isLoggedIn;
+  }
+}
