d96eb51a322ab413aa67d008d37528bffe1e4d40
juanpetterson <juan.petterson@gmail.com>
2020-03-31 21:47:41 -0300

    Implemented boards list, task and board dialog to add and remove them, added logged user avatar at shell component

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent implements OnInit {
-  isHandset$: Observable<boolean> = this.breakPointObserver
+export class ShellComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
     .observe([Breakpoints.Handset])
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakPointObserver: BreakpointObserver) {}
-
-  ngOnInit(): void {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public afAuth: AngularFireAuth
+  ) {}
 }
